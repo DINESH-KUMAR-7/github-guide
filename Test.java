b31241b26e213480dc4d13f17a20675f37f46490
D-K-7 <73380053+DINESH-KUMAR-7@users.noreply.github.com>
2022-08-07 17:22:47 +0530

    function printthestring is executed

--- Test.java
+++ Test.java
@@ -2,6 +2,7 @@ class Test{
     public static void main(String[] args) {
         int a=5,b=10;
         New nu = new New();
+        nu.printTheString("dk");
         System.out.println(a+b);
     }
 }
