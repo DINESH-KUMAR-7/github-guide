6ac635f70dcf1175731d7b783fb879cb838efc5f
DINESH-KUMAR-7 <dineshm0705@gmail.com>
2022-08-07 17:29:53 +0530

    string get/set method

---
+++ Text.java
@@ -0,0 +1,13 @@
+public class Text {
+    private String str;
+    
+    //set string
+    public void setStr(String s){
+        str = s;
+    }
+
+    //get string
+    public String getStr(){
+        return str;
+    }
+}
