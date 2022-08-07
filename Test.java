f1b79445e0fa1a04cb728f7b44e2a36f5e75c659
DINESH-KUMAR-7 <dineshm0705@gmail.com>
2022-08-07 17:35:34 +0530

    text class + updated files

--- Test.java
+++ Test.java
@@ -2,7 +2,10 @@ class Test{
     public static void main(String[] args) {
         int a=5,b=10;
         New nu = new New();
-        nu.printTheString("dk");
+        Text t = new Text();
+
+        t.setStr("dk");
+        nu.printTheString(t.getStr());
         System.out.println(a+b);
     }
 }
